fe62a58e68e0f9041f8d97a472ff9b1c50c63c21
chris <pachopa0320@gmail.com>
2018-01-20 07:26:46 +0000

    try to install Sass and can use bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <nav className="navbar navbar-light navbar-fixed-top navbarTop" id="my-navbar">
+          <div className="logo"><h2>Scortch</h2></div>
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle navSidebar" data-toggle="collapse" data-target="#navbarSidebar">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <button type="button" className="navbar-toggle user-border" data-toggle="collapse" data-target="#navbar-collapse">
+                <span className="user-icon">
+                  <i className="fa fa-user" aria-hidden="true"></i>
+                </span>
+              </button>
+              <a href="" className="navbar-brand navTitle">Homadsvadve</a>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }

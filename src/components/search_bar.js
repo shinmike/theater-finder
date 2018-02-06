d51bb9287eb395f1d920ce83b19eaf597190ff52
Michael Shin <shinmike90@gmail.com>
2018-02-05 19:26:35 -0800

    searchLocation setState

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      searchLocation: ''
+    }
+  }
+
+  updateLocation(e){
+    this.setState({
+      searchLocation: e.target.value
+    })
+  }
+
+  searchLocation(){
+    console.log(`searchLocation: ${this.state.searchLocation}`)
+  }
+
   render() {
     return (
       <div className="search-bar">
-        <input />
+        <input onChange={this.updateLocation.bind(this)} type="text" placeholder="" />
+        <button onClick={this.searchLocation.bind(this)}>Search</button>
       </div>
     );
   }
